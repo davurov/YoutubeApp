787929f415a1a306399c173e726394bdcbccfc58
davurov <113560218+davurov@users.noreply.github.com>
2023-03-26 16:27:42 +0500

    commit to test Bitrise

--- YoutubeApp/YoutubeApp/ViewController.swift
+++ YoutubeApp/YoutubeApp/ViewController.swift
@@ -12,7 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        let i = [1,2,3,4,5,6,7]
+        print(i[10])
     }
 
 
