995a3e29de956e639b0349d51b8a9ab5bdd6e063
davurov <113560218+davurov@users.noreply.github.com>
2023-03-26 15:37:30 +0500

    Initial commit

---
+++ YoutubeApp/YoutubeApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  YoutubeApp
+//
+//  Created by Abduraxmon on 26/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
